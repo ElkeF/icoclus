ed96ae41600027b47e87d4d4cdfbc6a5a9e93737
elke <elke.fasshauer@pci.uni-heidelberg.de>
2013-02-21 15:21:04 +0100

    create surface atoms, write output

--- icoclus.py
+++ icoclus.py
@@ -17,8 +17,10 @@ atouter = 'Ar' # atomtype of the outer shells
 rcore =  2.16 # radius of core atoms 
 router = 2.07 # radius of outer shell atoms
 
-n_core = 2 #number of atoms for the longest edge
+n_core = 3 #number of atoms for the longest edge
 n_outer = 0
+
+################## Definitions #####################################
 
 phi = (1 + math.sqrt(5))/2
 
@@ -47,9 +49,14 @@ surf20 = np.array([ 8,11,12])
 surfaces = np.vstack((surf1,surf2,surf3,surf4,surf5,surf6,surf7,\
                       surf8,surf9,surf10,surf11,surf12,surf13,\
                       surf14,surf15,surf16,surf17,surf18,surf19,surf20))
-print surfaces
+
+central = np.array([0,0,0])
+coords  = central
 
 ####################### Functions used #############################
+
+def vec2str(vec):
+    return "   ".join([str(v) for v in vec])
 
 ####################### Programme ##################################
 
@@ -60,9 +67,10 @@ sp      = np.vdot(testvec,avec)
 norm    = np.linalg.norm(avec)
 
 ##################################
-
-for i in (1,n_core):
-    kante  = rcore * (i-1)
+#####  Build the core icosahedra #
+##################################
+for i in range (2,n_core+1):
+    kante  = 2* rcore * (i-1)
 
 # Die Ecken des Ikosaeders der entsprechenden Groesse
     ecke1  = np.array([           0,    -kante/2, kante/2*phi])
@@ -80,4 +88,54 @@ for i in (1,n_core):
 
     ecken = np.vstack((ecke1,ecke2,ecke3,ecke4,ecke5,ecke6,ecke7,\
                        ecke8,ecke9,ecke10,ecke11,ecke12))
-    print ecken[11]
+
+    latest = ecken
+#    print latest
+
+    for j in range (0,19):
+        vec1 = ecken[surfaces[j,0] -1]
+        vec2 = ecken[surfaces[j,1] -1]
+        vec3 = ecken[surfaces[j,2] -1]
+
+#        print ' '.join(map(str, vec1))
+#        print ' '.join(map(str, vec2))
+#        print ' '.join(map(str, vec3))
+        normkante = (vec2-vec1) / np.linalg.norm(vec2-vec1)
+        normlauf  = (vec3-vec2) / np.linalg.norm(vec3-vec2)
+
+        for k in range (1,i-1):
+            kantatom = vec1 + i * normkante * 2 * rcore
+            latest = np.vstack((latest,kantatom))
+            
+            for l in range (1,k):
+                flatom = kantatom + k * normlauf * 2 * rcore
+                latest = np.vstack((latest,flatom))
+
+#    print latest
+    coords = np.vstack((coords,latest))
+
+#########################################
+# Write Output
+#########################################
+
+
+
+
+xyz_1st  = [vec2str(coord) for coord in coords]
+
+lines_1st = []
+for coord in xyz_1st:
+    line = '%s    %s' %(atcore,coord)
+    lines_1st.append(line)
+print_1st = '\n'.join(lines_1st)
+
+
+outlist = [print_1st]
+
+#print outlist
+outlines = '\n'.join(outlist)
+#print outlines
+
+outfile = open("%s%s_ico.xyz" %(atouter,atcore), mode="w")
+outfile.writelines(outlines)
+
